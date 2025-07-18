fadf6e796faea8537e5aa1319444bfefe35605e9
Lucas Ribas Cardoso <lucas.rib.card@gmail.com>
2025-07-18 16:19:17 -0300

    chore: initialize Spring Boot project with Maven setup and essential configurations

---
+++ src/main/java/com/LucasRibasCardoso/api_rest_with_spring_boot/ApiRestWithSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.LucasRibasCardoso.api_rest_with_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestWithSpringBootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ApiRestWithSpringBootApplication.class, args);
+  }
+}
