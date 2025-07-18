e8d88d84d0bc22de2b5ed1cb553d3d1c356e0244
Lucas Ribas Cardoso <lucas.rib.card@gmail.com>
2025-07-18 16:30:54 -0300

    chore: add initial test class, application configuration, and Docker setup

---
+++ src/test/java/com/LucasRibasCardoso/api_rest_with_spring_boot/ApiRestWithSpringBootApplicationTests.java
@@ -0,0 +1,11 @@
+package com.LucasRibasCardoso.api_rest_with_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestWithSpringBootApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
