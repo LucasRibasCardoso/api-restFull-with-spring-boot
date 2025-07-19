9b6fcc7bec6b6f552413f1e3fda660147c642a02
Lucas Ribas Cardoso <lucas.rib.card@gmail.com>
2025-07-18 21:55:02 -0300

    feat: create migration for populate table tb_person

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.tb_person (id, first_name, last_name, cpf, gender)
+VALUES (1, 'Maria', 'Silva Santos', '987.654.321-11', 'F'),
+       (4, 'Mariana', 'Costa', '111.222.333-44', 'F');
