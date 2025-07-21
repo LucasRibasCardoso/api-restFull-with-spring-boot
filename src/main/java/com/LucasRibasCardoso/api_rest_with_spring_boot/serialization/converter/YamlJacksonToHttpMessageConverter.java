b1fba9ca6d09d3d05cfcf034d002d5da70cbfdc1
LucasRibasCardoso <lucas.rib.card@gmail.com>
2025-07-21 19:48:46 -0300

    feat: add YAML support for content negotiation and serialization

---
+++ src/main/java/com/LucasRibasCardoso/api_rest_with_spring_boot/serialization/converter/YamlJacksonToHttpMessageConverter.java
@@ -0,0 +1,15 @@
+package com.LucasRibasCardoso.api_rest_with_spring_boot.serialization.converter;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+
+public final class YamlJacksonToHttpMessageConverter extends AbstractJackson2HttpMessageConverter {
+
+    protected YamlJacksonToHttpMessageConverter() {
+      super(new YAMLMapper()
+          .setSerializationInclusion(JsonInclude.Include.NON_NULL),
+          MediaType.parseMediaType("application/yaml"));
+    }
+}
