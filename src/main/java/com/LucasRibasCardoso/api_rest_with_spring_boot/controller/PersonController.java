15ee4e3332e5e506182460a5dd5a38d2386deb5e
LucasRibasCardoso <lucas.rib.card@gmail.com>
2025-07-22 23:15:55 -0300

    feat: Add Swagger Documentation in PersonController

--- src/main/java/com/LucasRibasCardoso/api_rest_with_spring_boot/controller/PersonController.java
+++ src/main/java/com/LucasRibasCardoso/api_rest_with_spring_boot/controller/PersonController.java
@@ -1,9 +1,11 @@
 package com.LucasRibasCardoso.api_rest_with_spring_boot.controller;
 
+import com.LucasRibasCardoso.api_rest_with_spring_boot.docs.PersonControllerDocs;
 import com.LucasRibasCardoso.api_rest_with_spring_boot.dto.person.PersonCreateDto;
 import com.LucasRibasCardoso.api_rest_with_spring_boot.dto.person.PersonResponseDto;
 import com.LucasRibasCardoso.api_rest_with_spring_boot.dto.person.PersonUpdateDto;
 import com.LucasRibasCardoso.api_rest_with_spring_boot.service.PersonService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.net.URI;
 import java.util.List;
@@ -13,19 +15,25 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+@Tag(name = "People", description = "Endpoints for managing people")
 @RestController
 @RequestMapping("/api/v1/person")
-public class PersonController {
+public class PersonController implements PersonControllerDocs {
 
-  @Autowired private PersonService service;
+  private final PersonService service;
+
+  public PersonController(PersonService service) {
+    this.service = service;
+  }
 
   @GetMapping(
       value = "/{id}",
       produces = {
         MediaType.APPLICATION_JSON_VALUE,
-        MediaType.APPLICATION_XML_VALUE,
-        MediaType.APPLICATION_YAML_VALUE
+        MediaType.APPLICATION_YAML_VALUE,
+        MediaType.APPLICATION_XML_VALUE
       })
+  @Override
   public ResponseEntity<PersonResponseDto> findById(@PathVariable Long id) {
     return ResponseEntity.ok(service.getById(id));
   }
@@ -33,9 +41,10 @@ public class PersonController {
   @GetMapping(
       produces = {
         MediaType.APPLICATION_JSON_VALUE,
-        MediaType.APPLICATION_XML_VALUE,
-        MediaType.APPLICATION_YAML_VALUE
+        MediaType.APPLICATION_YAML_VALUE,
+        MediaType.APPLICATION_XML_VALUE
       })
+  @Override
   public ResponseEntity<List<PersonResponseDto>> findAll() {
     return ResponseEntity.ok(service.getAll());
   }
@@ -43,14 +52,15 @@ public class PersonController {
   @PostMapping(
       consumes = {
         MediaType.APPLICATION_JSON_VALUE,
-        MediaType.APPLICATION_XML_VALUE,
-        MediaType.APPLICATION_YAML_VALUE
+        MediaType.APPLICATION_YAML_VALUE,
+        MediaType.APPLICATION_XML_VALUE
       },
       produces = {
         MediaType.APPLICATION_JSON_VALUE,
-        MediaType.APPLICATION_XML_VALUE,
-        MediaType.APPLICATION_YAML_VALUE
+        MediaType.APPLICATION_YAML_VALUE,
+        MediaType.APPLICATION_XML_VALUE
       })
+  @Override
   public ResponseEntity<PersonResponseDto> create(@RequestBody PersonCreateDto personCreateDto) {
     PersonResponseDto dto = service.save(personCreateDto);
     URI location =
@@ -63,22 +73,24 @@ public class PersonController {
 
   @PatchMapping(
       value = "/{id}",
-      consumes = {
-        MediaType.APPLICATION_JSON_VALUE,
-        MediaType.APPLICATION_XML_VALUE,
-        MediaType.APPLICATION_YAML_VALUE
-      },
       produces = {
         MediaType.APPLICATION_JSON_VALUE,
-        MediaType.APPLICATION_XML_VALUE,
-        MediaType.APPLICATION_YAML_VALUE
+        MediaType.APPLICATION_YAML_VALUE,
+        MediaType.APPLICATION_XML_VALUE
+      },
+      consumes = {
+        MediaType.APPLICATION_JSON_VALUE,
+        MediaType.APPLICATION_YAML_VALUE,
+        MediaType.APPLICATION_XML_VALUE
       })
+  @Override
   public ResponseEntity<PersonResponseDto> update(
       @PathVariable Long id, @RequestBody @Valid PersonUpdateDto personUpdateDto) {
     return ResponseEntity.ok(service.update(id, personUpdateDto));
   }
 
   @DeleteMapping("/{id}")
+  @Override
   public ResponseEntity<Void> delete(@PathVariable Long id) {
     service.delete(id);
     return ResponseEntity.noContent().build();
