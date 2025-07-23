eb22d5c3b00ddda2e423820bebc397b8fec833b5
LucasRibasCardoso <lucas.rib.card@gmail.com>
2025-07-23 14:45:07 -0300

    refactor: simplify person table creation and population scripts

--- src/main/resources/db/migration/V3__Sync_person_id_sequence.sql
+++ src/main/resources/db/migration/V3__Sync_person_id_sequence.sql
@@ -1,9 +1,2 @@
+SELECT setval(pg_get_serial_sequence('tb_person','id'), (SELECT MAX(id) FROM tb_person));
 
-
--- V3__Sync_person_id_sequence.sql
--- Ajusta a sequência do id na tabela TB_PERSON após populações manuais
-
-SELECT setval(
-  pg_get_serial_sequence('TB_PERSON', 'id'),
-  (SELECT MAX(id) FROM TB_PERSON)
-);
