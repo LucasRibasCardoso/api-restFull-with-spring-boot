946ff3606466322e0c99c1533cb7c7a48f872e64
LucasRibasCardoso <lucas.rib.card@gmail.com>
2025-07-25 15:18:00 -0300

    feat: add CORS configuration

--- src/main/java/com/LucasRibasCardoso/api_rest_with_spring_boot/config/WebConfig.java
+++ src/main/java/com/LucasRibasCardoso/api_rest_with_spring_boot/config/WebConfig.java
@@ -1,12 +1,17 @@
 package com.LucasRibasCardoso.api_rest_with_spring_boot.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+  @Value("${cors.originPatterns:default}")
+  private String corsOriginPatterns;
 
   @Override
   public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
@@ -18,5 +23,16 @@ public class WebConfig implements WebMvcConfigurer {
         .mediaType("json", MediaType.APPLICATION_JSON)
         .mediaType("xml", MediaType.APPLICATION_XML)
         .mediaType("yaml", MediaType.APPLICATION_YAML);
+  }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    var allowedOrigins = corsOriginPatterns.split(",");
+
+    registry
+        .addMapping("/api/v1/**")
+        .allowedOrigins(allowedOrigins)
+        .allowedMethods("GET", "POST", "DELETE", "PATCH", "OPTIONS")
+        .allowCredentials(true);
   }
 }
